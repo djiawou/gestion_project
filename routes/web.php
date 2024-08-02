94eac09e182ccbca3591eaae17329671fb423c27
djiawou <amourakowakou@gmail.com>
2024-08-02 14:51:27 +0200

    page d'aceuil

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::resource('projects', ProjectController::class);
 Route::resource('tasks', TaskController::class);
